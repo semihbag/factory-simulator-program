93bc50f01bfc33fdb6d3d4241325bebfd49bd5c3
semihbag <105243010+semihbag@users.noreply.github.com>
2022-10-06 00:18:24 +0300

    Add files via upload

---
+++ Item.java
@@ -0,0 +1,35 @@
+//Name: Semih
+//Surname: Bað
+//Student ID:150120070
+
+
+// Define the item class
+public class Item {
+	private int id;
+	public static int numberOfItems;
+	
+	// Construct a item with a specified id
+	public Item(int id) {
+		this.id = id;
+		numberOfItems++;
+	}
+	
+	
+	/** Get and set methods */
+	
+	// Return the id of this item
+	public int getId() {
+		return id;
+	}
+	// Set a new id for this item
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	
+	// Return the total number of items
+	public static int getNumberOfItems() {
+		return numberOfItems;
+	}
+}
